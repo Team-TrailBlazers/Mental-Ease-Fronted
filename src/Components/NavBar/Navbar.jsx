43623924d7fdb1e959464a9bf1eb8d73f2d41485
kemboi590 <bkemboi590@gmail.com>
2024-01-31 12:51:26 +0300

    responsive nav bar done

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,25 +1,30 @@
-import React from "react";
-import "./navbar.css"
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-function Navbar() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/home">Home</Link>
-        </li>
-        <li>
-          <Link to="/stripe">Stripe</Link>
-        </li>
-        <li>
-          <Link to="/success">Success</Link>
-        </li>
-        <li>
-          <Link to="/auth/register">Register</Link>
-        </li>
+import { FiAlignJustify } from "react-icons/fi";
+import { GrClose } from "react-icons/gr";
+import "./navbar.css";
 
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return ( 
+    <nav>
+      <div onClick={() => setIsOpen(!isOpen)} className="burger">
+        {isOpen ? <GrClose  className="close"/> : <FiAlignJustify className="bars"/>}
+      </div>
+      <ul className={isOpen ? 'open' : ''}>
         <li>
-          <Link to="/auth/login">Login</Link>
+          <Link to="/home" onClick={() => setIsOpen(false)} style={{color: "white"}}>Home</Link>
+        </li>
+        <li>
+          <Link to="/stripe" onClick={() => setIsOpen(false)} style={{color: "white"}}>Stripe</Link>
+        </li>
+        <li>
+          <Link to="/success" onClick={() => setIsOpen(false)} style={{color: "white"}}>Success</Link>
+        </li>
+        <li>
+          <Link to="/auth/register" onClick={() => setIsOpen(false)} style={{color: "white"}} >Register</Link>
         </li>
       </ul>
     </nav>
