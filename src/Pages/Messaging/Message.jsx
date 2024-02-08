4a276db8603a67a29a74e4aff41c7de5a907e477
kemboi590 <bkemboi590@gmail.com>
2024-02-08 14:57:11 +0300

    ready to revert

---
+++ src/Pages/Messaging/Message.jsx
@@ -0,0 +1,44 @@
+import "./message.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "../Chat";
+
+const socket = io.connect("http://localhost:8082");
+
+function Message() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <div className="Message">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Chat With Therapist</h3>
+          <input type="text" placeholder="Name..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input type="text" placeholder="Session ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join Chat</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
+}
+
+export default Message;
